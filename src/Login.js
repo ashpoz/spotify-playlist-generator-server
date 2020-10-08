1856c00e3b8409fb1ecb46f66609f8a9007430c8
hellomaryjo <majorx3music@gmail.com>
2020-10-08 01:23:57 -0500

    get spotify api working, add spotify lib and testing artist genre search

--- src/Login.js
+++ src/Login.js
@@ -1,149 +1,92 @@
 import React from 'react';
-import $ from "jquery";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
+
 
 
 class Login extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-          loggedIn: false,
-          error: null
-        };
-      }
-
-       getHashParams = () => {
-        var hashParams = {};
-        var e, r = /([^&;=]+)=?([^&;]*)/g,
-            q = window.location.hash.substring(1);
-        while ( e = r.exec(q)) {
-           hashParams[e[1]] = decodeURIComponent(e[2]);
-        }
-        return hashParams;
-      }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        fetch(`/login`, {
-          mode: 'no-cors',
-        })
-          .then(response => response)
-          .then(
-            () => {
-              this.setState({
-                loggedIn: true
-              });
-            },
-            (error) => {
-              console.log(error);
-              this.setState({
-                loggedIn: false,
-                error
-              });
-            }
-          )
-      }
-
-    componentDidMount() {
-            /**
-             * Obtains parameters from the hash of the URL
-             * @return Object
-             */
-
-            const getHashParams = () => {
-              var hashParams = {};
-              var e, r = /([^&;=]+)=?([^&;]*)/g,
-                  q = window.location.hash.substring(1);
-              while ( e = r.exec(q)) {
-                 hashParams[e[1]] = decodeURIComponent(e[2]);
-              }
-              return hashParams;
-            }
-            // var userProfileSource = document.getElementById('user-profile-template').innerHTML;
-
-                // userProfileTemplate = Handlebars.compile(userProfileSource),
-                // userProfilePlaceholder = document.getElementById('user-profile');
-    
-            // var oauthSource = document.getElementById('oauth-template').innerHTML;
-
-                // oauthTemplate = Handlebars.compile(oauthSource),
-                // oauthPlaceholder = document.getElementById('oauth');
-    
-            var params = getHashParams();
-    
-            var access_token = params.access_token,
-                refresh_token = params.refresh_token,
-                error = params.error;
-
-    
-            if (error) {
-              alert('There was an error during the authentication');
-              
-            } else {
-              if (access_token) {
-                // render oauth info
-                // oauthPlaceholder.innerHTML = oauthTemplate({
-                //   access_token: access_token,
-                //   refresh_token: refresh_token
-                // });
-    
-                $.ajax({
-                    url: 'https://api.spotify.com/v1/me',
-                    headers: {
-                      'Authorization': 'Bearer ' + access_token
-                    },
-                    success: function(response) {
-                    //   userProfilePlaceholder.innerHTML = userProfileTemplate(response);
-                      console.log(response)
-    
-                      $('#login').hide();
-                      $('#loggedin').show();
-                    }
-                });
-              } else {
-                  // render initial screen
-                  $('#login').show();
-                  $('#loggedin').hide();
-              }
-    
-              document.getElementById('obtain-new-token').addEventListener('click', function() {
-                $.ajax({
-                  url: '/refresh_token',
-                  data: {
-                    'refresh_token': refresh_token
-                  }
-                }).done(function(data) {
-                  access_token = data.access_token;
-                //   oauthPlaceholder.innerHTML = oauthTemplate({
-                //     access_token: access_token,
-                //     refresh_token: refresh_token
-                //   });
-                });
-              }, false);
-            }
-          }
-
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div id="login">
-                            <h1>This is an example of the Authorization Code flow</h1>
-                            <a onClick={this.handleSubmit} href="/login" className="btn btn-primary">Log in with Spotify</a>
-                        </div>
-                    </div>
-                </div>
-            <div id="loggedin">
-                <div id="user-profile">
-                </div>
-                <div id="oauth">
-                </div>
-                <button className="btn btn-default" id="obtain-new-token">Obtain new token using the refresh token</button>
-            </div>
-            </div>
-          );
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
     }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' },
+      artists: { items: [] }
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            albumArt: response.item.album.images[0].url
+          }
+        });
+      })
+  }
+
+  searchGenres() {
+    console.log(spotifyApi);
+    spotifyApi.search("genre:Rock", ["artist"])
+    .then((response) => {
+      console.log(response);
+      // this.setState({
+      //   artists: {
+      //     items: response.items.artists,
+      //   }
+      // });
+    })
+  }
+
+
+  // componentDidMount() {
+
+  // }
+
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div id="login">
+              <h1>This is an example of the Authorization Code flow</h1>
+              <a href="http://localhost:5000/login" className="btn btn-primary">Log in with Spotify</a>
+              {this.state.loggedIn &&
+                <>
+                <button onClick={() => this.getNowPlaying()}>
+                  Check Now Playing
+                </button>
+                <button onClick={() => this.searchGenres()}>Search genres</button>
+                </>
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
