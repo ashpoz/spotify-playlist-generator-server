bbe9226826c46cd968eb3d3e3e953d1d62951746
hellomaryjo <majorx3music@gmail.com>
2023-03-30 10:15:30 -0500

    update nav with new github url

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -6,13 +6,13 @@ import githubLogo from "./images/github-logo-white.png";
 
 function Nav() {
     return (
-    <nav className="navbar navbar-dark">
-        <a className="navbar-brand" href="/">Spotify Playlist Generator</a>
-        <a className="mr-sm-2" href="https://github.com/hellomaryjo" target="_blank" rel="noopener noreferrer">
-            <img className="social-icon" src={githubLogo} alt="Github Logo" />
-        </a>
-    </nav>
+        <nav className="navbar navbar-dark">
+            <a className="navbar-brand" href="/">Spotify Playlist Generator</a>
+            <a className="mr-sm-2" href="https://github.com/ashpoz" target="_blank" rel="noopener noreferrer">
+                <img className="social-icon" src={githubLogo} alt="Github Logo" />
+            </a>
+        </nav>
     )
-  }
+}
 
 export default Nav;
